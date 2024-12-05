ded088eae6e34d1412f94abde292d523eddb3b96
anstonebear <anstonebear@mail.ru>
2024-12-05 19:29:51 +0300

    update

--- src/helpers/ProjectsList.js
+++ src/helpers/ProjectsList.js
@@ -5,56 +5,64 @@ import project04 from '../img/projects/04.jpg';
 import project05 from '../img/projects/05.jpg';
 import project06 from '../img/projects/06.jpg';
 import project07 from '../img/projects/07.jpg';
+import project08 from '../img/projects/08.jpg';
 
 export const projects = [
 	{
 		id: 1,
-		title: 'Pizza sales React app',
-		skills:
-			'React, SCSS, Redux toolkit, typescript, JestJS, webpack, Charts.js',
-		githubLink: 'https://react-pizza-gules-three.vercel.app/',
+		title: 'Crypto wallet React app',
+		skills: 'React, SCSS, Ant Design, Charts.js',
+		githubLink: 'https://crypto-opal-mu.vercel.app/',
 		image: project01,
 	},
 	{
 		id: 2,
-		title: 'Calorie calculator JS app',
-		skills: 'javascript, HTML, CSS',
-		githubLink: 'https://anstonebear.github.io/Food.FullProject/',
+		title: 'Pizza sales React app',
+		skills:
+			'React, SCSS, Redux toolkit, typescript, JestJS, webpack, Charts.js',
+		githubLink: 'https://react-pizza-gules-three.vercel.app/',
 		image: project02,
 	},
 	{
 		id: 3,
-		title: 'React app for call handling',
-		skills: 'React, SCSS, Redux toolkit, typescript, Vite',
-		githubLink: 'https://tz-skilla.vercel.app/#',
+		title: 'Calorie calculator JS app',
+		skills: 'javascript, HTML, CSS',
+		githubLink: 'https://anstonebear.github.io/Food.FullProject/',
 		image: project03,
 	},
 	{
 		id: 4,
-		title: 'Character card JS app',
-		skills: 'javascript, HTML, CSS, axios',
-		githubLink: 'https://anstonebear.github.io/comics-ref/',
+		title: 'React app for call handling',
+		skills: 'React, SCSS, Redux toolkit, typescript, Vite',
+		githubLink: 'https://tz-skilla.vercel.app/#',
 		image: project04,
 	},
 	{
 		id: 5,
-		title: 'Hotel booking React app',
-		skills: 'React, SCSS, Redux toolkit, javascript, webpack',
-		githubLink: 'https://simple-hotel.vercel.app/mainpage',
+		title: 'Character card JS app',
+		skills: 'javascript, HTML, CSS, axios',
+		githubLink: 'https://anstonebear.github.io/comics-ref/',
 		image: project05,
 	},
 	{
 		id: 6,
-		title: 'Demo movie search app',
-		skills: 'javascript, HTML, CSS',
-		githubLink: 'https://anstonebear.github.io/movie/',
+		title: 'Hotel booking React app',
+		skills: 'React, SCSS, Redux toolkit, javascript, webpack',
+		githubLink: 'https://simple-hotel.vercel.app/mainpage',
 		image: project06,
 	},
 	{
 		id: 7,
+		title: 'Demo movie search app',
+		skills: 'javascript, HTML, CSS',
+		githubLink: 'https://anstonebear.github.io/movie/',
+		image: project07,
+	},
+	{
+		id: 8,
 		title: 'Page food delivery',
 		skills: 'HTML, CSS',
 		githubLink: 'https://anstonebear.github.io/UberFood/',
-		image: project07,
+		image: project08,
 	},
 ];
